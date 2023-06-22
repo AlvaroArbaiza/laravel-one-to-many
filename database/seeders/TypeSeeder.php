a7449408c8b18e2a787953f153916cd053d61e88
AlvaroArbaiza <alvaro.xander@gmail.com>
2023-06-22 16:28:13 +0200

    Riempimento colonne della tabella types

--- database/seeders/TypeSeeder.php
+++ database/seeders/TypeSeeder.php
@@ -5,6 +5,9 @@ namespace Database\Seeders;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
+use App\Models\Admin\Type;
+use Illuminate\Support\Str;
+
 class TypeSeeder extends Seeder
 {
     /**
@@ -14,6 +17,18 @@ class TypeSeeder extends Seeder
      */
     public function run()
     {
-        //
+        $types = [
+            'FrontEnd',
+            'Backend',
+            'FullStack',
+            'Design'
+        ];
+
+        foreach ($types as $type) {
+            $new_type = new Type();
+            $new_type->name = $type;
+            $new_type->slug = Str::slug( $new_type->name , '-');
+            $new_type->save();
+        }
     }
 }
